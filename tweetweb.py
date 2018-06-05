56e53744b1901f7139c62cfc95d8572932112e3f
Katie Maxwell <katie@codeinstitute.net>
2018-06-05 14:05:33 +0000

    wired search to text box

--- tweetweb.py
+++ tweetweb.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from search_tweets import search
 
 app = Flask(__name__)
 
@@ -9,7 +10,9 @@ def get_index():
     
 @app.route("/results")
 def get_results():
-    return render_template('results.html')
+    topic = request.args.get('search')
+    tweets = search(topic,10)
+    return render_template('results.html', tweets=tweets)
 
 if __name__ == '__main__':
     app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)), debug=True)
