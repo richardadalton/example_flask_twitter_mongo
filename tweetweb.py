1af5781e8b6b7a5f29d425d61671756e3ac783ea
Richard Dalton <richard@codeinstitute.net>
2018-06-05 13:45:25 +0000

    Made results page dynamic

--- tweetweb.py
+++ tweetweb.py
@@ -9,7 +9,11 @@ def get_index():
     
 @app.route("/results")
 def get_results():
-    return render_template('results.html')
+    results = [
+        {'id': 1003934266038849536, 'text': 'Hello'},
+        {'id': 1003995254486282240, 'text': 'This is another tweet'},
+        ]
+    return render_template('results.html', tweets=results)
 
 if __name__ == '__main__':
     app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)), debug=True)
