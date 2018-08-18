271696db860329bb56d1d143898e9a4b60e4a324
Richard Dalton <richard@codeinstitute.net>
2018-08-18 08:10:07 +0000

    Restyled UI

--- tweetweb.py
+++ tweetweb.py
@@ -28,7 +28,7 @@ def get_results():
             tweets = search_tweets(query, count)
             mongo.db[query].insert_many(tweets)
 
-    return render_template('results.html', tweets=tweets)
+    return render_template('index.html', tweets=tweets)
 
 if __name__ == '__main__':
     app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)), debug=True)
